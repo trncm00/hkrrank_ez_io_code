eb6483e4420392a025ceaa906c2a82c9b649120b
trncm00 <36527137+trncm00@users.noreply.github.com>
2021-07-01 17:21:42 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,10 +7,45 @@ using namespace std;
 
 
 int main() {
-    
 
-    
-    vector<int> vect_array= {1,2,7};
+//the top one wins all the test cases,
+//but it hangs in clion and I am not sure why
+//tandon ta's felt clion does
+//stuff for users other compilers would (justifiably) force you to manage yourself.
+
+//so a,b,c are initialized to 1,2,7
+//cin bit shift operator int variable 3x
+//i guess the compiler moves variables into
+// memory,
+//( memory you have not been forced to define yourself mind you, like with pointers and new)
+// and when it jumps to cout
+//tje compiler adds them all up nice and neat
+    int a =1;
+    int b =2;
+    int c = 7;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+    /*
+    int a = 1;
+    int b = 2;
+    int c = 7;
+
+    cin >>a;
+    cin >> b;
+    cin >> c;
+    cout<< a + b + c;
+    */
+
+    /*
+    int a = 1;
+    int b = 2;
+    int c = 7;
+
+    int expression = a + b + c;
+
+cout << a + b + c;
+*/
+  /*  vector<int> vect_array= {1,2,7};
     int n = 0;
 
     for(int i =0; i<vect_array.size();i++){
@@ -18,17 +53,9 @@ int main() {
 
     }
     cout << n << endl;
-    
-    /*int a = 1;
-    int b = 2;
-    int c = 7;
-
-    int expression = a + b + c;
-
-cout << a + b + c;*/
-    
     return 0;
+*/
 
-
-   }
+return 0;
+}
 

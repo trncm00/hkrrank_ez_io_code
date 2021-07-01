2cd644dfbb31948f10edba5ec9b2e68477595876
trncm00 <36527137+trncm00@users.noreply.github.com>
2021-07-01 18:23:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main() {
     //i don't understand stdin and stdout yet, (forgive me nyu instructors i know you tried)
     // but basically cin wasn't hanging so much as waiting for input
     // cin is NOT MEANT for hardcoded values I guess, just keyboard and file input
+ //i c and p'd the whole q and a session below the main scope if you wanna see,
+ //i am keeping it for my own understanding
     
  
 //tandon ta's felt clion does
@@ -67,4 +69,92 @@ cout << a + b + c;
 
 return 0;
 }
+/*
+Jowohn
+ — 
+Today at 5:57 PM
+how come you're initializing a, b, and c to specific values if you're gonna input values into them immediately after
+tm_square_newb
+ — 
+Today at 6:00 PM
+i figure they have to be initialized to some value, otherwise an error is thrown back and, to solve the problem I must  output 10 in the console
+idk C++ well enough to get at your question Jowohn
+killerbee13
+ — 
+Today at 6:00 PM
+When you run it does it appear in a separate console or is it in the clion output view?
+tm_square_newb
+ — 
+Today at 6:00 PM
+latter
+in the clion output view
+killerbee13
+ — 
+Today at 6:01 PM
+I'm not sure if you can type input into that
+tm_square_newb
+ — 
+Today at 6:01 PM
+wait, cin is meant specifically for user input and not hard-coded values?
+am i thinking about this correctly?
+because I am not typing 1,2,7 into the console here
+killerbee13
+ — 
+Today at 6:02 PM
+Yes, cin is for accepting input
+tm_square_newb
+ — 
+Today at 6:02 PM
+hm, ok
+killerbee13
+ — 
+Today at 6:03 PM
+Your program expects 3 integers with spaces/newlines between them
+Jowohn
+ — 
+Today at 6:03 PM
+on online compilers they often automatically input stuff into your program when you run it to see if it works
+esp for like
+problem set websites
+tm_square_newb
+ — 
+Today at 6:04 PM
+so the compiler sees 
+
+cin
+
+ and expects 1 2 7
+not three variables holding 1 2 7
+Jowohn
+ — 
+Today at 6:05 PM
+it's gonna stop what it's doing until someone inputs numbers into the console and presses enter
+and then put those numbers into a, b, c
+tm_square_newb
+ — 
+Today at 6:05 PM
+oohhhh
+Jowohn
+ — 
+Today at 6:05 PM
+and it's not technically the compiler that's stopping, but that's aside the point
+killerbee13
+ — 
+Today at 6:06 PM
+The values you set for a,b,c are not used after that line
+tm_square_newb
+ — 
+Today at 6:06 PM
+thanks folks, I sorta see now, it's expecting input
+and its dropped a b c
+killerbee13
+ — 
+Today at 6:06 PM
+Unless you type like a letter in and it goes into a fail state
+tm_square_newb
+ — 
+Today at 6:06 PM
+hm wow
+
+*/
 
